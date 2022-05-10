08015b2a71cf64c83f28cb09750a633250017ef8
Paul Beard <pbeardey@gmail.com>
2022-05-10 21:51:44 +0100

    readers CRUD - read, readById and patchById pass

--- src/controllers/readers.js
+++ src/controllers/readers.js
@@ -5,6 +5,52 @@ const { Reader } = require('../models');
 
 exports.createReader = async (req, res)  => {
    const newReader = await Reader.create(req.body);
-   res.status(201).json(newReader);
-}
 
+   res.status(201).json(newReader);
+};
+
+//GET all readers records
+exports.readReaders = async (req, res) => {
+   const readers = await Reader.findAll();
+  
+   try {
+      res.status(200).json(readers);
+   } catch(err) {
+      res.status(500).json(err);
+   }
+};
+
+//GET reader by Id
+exports.readReaderById = async (req, res) => {
+   const { readerId } = req.params;
+
+   const reader = await Reader.findByPk(readerId);
+
+   try{
+      if(!reader) {
+         res.status(404).json({ error : "The reader could not be found." });
+      } else {
+         res.status(200).json(reader);
+      }
+   } catch(err) {
+      res.status(500).json(err);
+   }
+};
+
+//PATCH reader by Id
+exports.patchReaderById = async (req, res) => {
+   const { readerId } = req.params;
+   const data = req.body;
+
+   const [ affectedRows ] = await Reader.update(data, { where: {id: readerId} } );
+   try{
+      if(!affectedRows) {
+         res.status(404).json({ error : "The reader could not be found." });
+      } else {
+         res.status(200).json(affectedRows);
+      }
+   } catch(err) {
+      res.status(500).json(err);
+   }
+
+}
--- src/routes/readers.js
+++ src/routes/readers.js
@@ -1,10 +1,15 @@
 // src/routes/readers.js
 
 const express = require('express');
+//const { READUNCOMMITTED } = require('sequelize/types/table-hints');
 const router = express.Router();
 
 const readersController = require('../controllers/readers');
 
+router.get ('/', readersController.readReaders);
+router.get ('/:readerId', readersController.readReaderById);
 router.post ('/', readersController.createReader);
+router.patch ('/:readerId', readersController.patchReaderById);
+
 
 module.exports = router;
